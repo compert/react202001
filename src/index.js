d1cbfeb94492b8c921696b981a1cb8332a4c44bf
gschoi <gschoi@enliple.com>
2020-02-13 10:28:50 +0900

    샘플 파일 정리

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 
 import configureStore from './redux/store'
-import App from './pages/samples/Routes/App'
+import App from '../stories/Routes/App'
 
 const store = configureStore(undefined)
 
--- src/pages/samples/Todo/TodoTitle/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import styles from './styles.scss'
-
-const TodoTitle = () => {
-  return <div className={styles.wrapper}>React Todo List</div>
-}
-
-export default TodoTitle
